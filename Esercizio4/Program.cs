c9549e2a13c513f5c8ec27a385120845f6301f16
CArnaboldi <arnaboldi@triminformatica.onmicrosoft.com>
2024-02-13 18:07:52 +0100

    esempio convertitore numeri romani

--- Esercizio4/Program.cs
+++ Esercizio4/Program.cs
@@ -41,49 +41,97 @@ do
     {
         Console.WriteLine("importo inserito non valido");
     }
-    Console.WriteLine("Inserisci un numero da 1 a 100");
+    Console.WriteLine("Inserisci un numero da 1 a 1000");
     stringaAcquisita = Console.ReadLine();
     count++;
 }
 while (!Int32.TryParse(stringaAcquisita, out numeroAcquisito) || numeroAcquisito > 1000 || numeroAcquisito < 1);
 
 Console.WriteLine("Numero inserito: " + numeroAcquisito);
-List<string> numeriRomani = new List<string>()
-{
-    "I", "V", "X", "L", "C", "D", "M"
-};
+//List<string> numeriRomani = new List<string>()
+//{
+//    "I", "V", "X", "L", "C", "D", "M"
+//};
 
-var div10 = 100;
-var posLettera = 4;
-while(numeroAcquisito > 0)
+//var div10 = 100;
+//var posLettera = 4;
+//while(numeroAcquisito > 0)
+//{
+//    if(numeroAcquisito == 1000)
+//    {
+//        numeroConvertito = numeriRomani[6];
+//        break;
+//    }
+    
+//    var num = numeroAcquisito / div10;
+//    numeroAcquisito %= div10;
+    
+//    if(num == 9)
+//    {
+//        numeroConvertito += numeriRomani[posLettera] + numeriRomani[posLettera + 2];
+//        num -= 9;
+//    }
+//    else if(num >= 5)
+//    {
+//        numeroConvertito += numeriRomani[posLettera+1];
+//        num -= 5;
+//    }
+//    else if(num == 4)
+//    {
+//        numeroConvertito += numeriRomani[posLettera] + numeriRomani[posLettera+1];
+//        num -= 4;
+//    }
+//    numeroConvertito += string.Concat(Enumerable.Repeat(numeriRomani[posLettera], num));
+//    div10 /= 10;
+//    posLettera -= 2;
+//}
+
+//Console.WriteLine("Numero convertito: " + numeroConvertito);
+
+var dictValoriCorrispondenze = new Dictionary<int, string>();
+
+dictValoriCorrispondenze[1] = "I";
+dictValoriCorrispondenze[4] = "IV";
+dictValoriCorrispondenze[5] = "V";
+dictValoriCorrispondenze[9] = "IX";
+dictValoriCorrispondenze[10] = "X";
+dictValoriCorrispondenze[40] = "XL";
+dictValoriCorrispondenze[50] = "L";
+dictValoriCorrispondenze[90] = "XC";
+dictValoriCorrispondenze[100] = "C";
+dictValoriCorrispondenze[400] = "CD";
+dictValoriCorrispondenze[500] = "D";
+dictValoriCorrispondenze[900] = "CM";
+dictValoriCorrispondenze[1000] = "M";
+
+int resto = numeroAcquisito;
+
+string risultato = "";
+
+while(resto > 0)
 {
-    if(numeroAcquisito == 1000)
-    {
-        numeroConvertito = numeriRomani[6];
-        break;
-    }
-    
-    var num = numeroAcquisito / div10;
-    numeroAcquisito %= div10;
-    
-    if(num == 9)
-    {
-        numeroConvertito += numeriRomani[posLettera] + numeriRomani[posLettera + 2];
-        num -= 9;
-    }
-    else if(num >= 5)
-    {
-        numeroConvertito += numeriRomani[posLettera+1];
-        num -= 5;
-    }
-    else if(num == 4)
-    {
-        numeroConvertito += numeriRomani[posLettera] + numeriRomani[posLettera+1];
-        num -= 4;
-    }
-    numeroConvertito += string.Concat(Enumerable.Repeat(numeriRomani[posLettera], num));
-    div10 /= 10;
-    posLettera -= 2;
+    var valMassimo = Utils.GetValoreMassimoFromDict(resto, dictValoriCorrispondenze);
+
+    resto -= valMassimo;
+
+    risultato += dictValoriCorrispondenze[valMassimo];
 }
 
-Console.WriteLine("Numero convertito: " + numeroConvertito);
+Console.WriteLine(risultato);
+Console.ReadLine();
+
+public static class Utils
+{
+    public static int GetValoreMassimoFromDict(int resto, Dictionary<int, string> dictValori)
+    {
+        foreach(var kvp in dictValori.Reverse())
+        {
+            if (kvp.Key <= resto)
+            {
+                return kvp.Key;
+            }
+        }
+
+        throw new Exception("c'è qualcosa che è andato storto..");
+    }
+}
